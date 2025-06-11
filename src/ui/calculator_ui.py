d3ea44e277f666cba947f63d86c960328681ec44
Heitor <heitormanoel05@gmail.com>
2025-06-11 14:31:56 -0300

    update calculator desing

--- src/ui/calculator_ui.py
+++ src/ui/calculator_ui.py
@@ -10,7 +10,7 @@ class CalculatorUI(ctk.CTkFrame):
         for j in range(4):
             self.columnconfigure(j, weight=1)
 
-        self.display = ctk.CTkEntry(self, font=("Arial", 24))
+        self.display = ctk.CTkEntry(self, font=("Arial", 24), height=80)
         self.display.grid(row=0, column=0, columnspan=4, sticky="nsew", pady=10, padx=5)
         self._criar_botoes()
 
@@ -19,7 +19,7 @@ class CalculatorUI(ctk.CTkFrame):
             ("7", 1, 0), ("8", 1, 1), ("9", 1, 2), ("⌫", 1, 3),
             ("4", 2, 0), ("5", 2, 1), ("6", 2, 2), ("C", 2, 3),
             ("1", 3, 0), ("2", 3, 1), ("3", 3, 2), ("-", 3, 3),
-            ("0", 4, 0), ("/", 4, 1), ("*", 4, 2), ("+", 4, 3),
+            ("/", 4, 0), ("0", 4, 1), ("*", 4, 2), ("+", 4, 3),
             ("√", 5, 0), ("^", 5, 1), (".", 5, 2), ("=", 5, 3)
         ]
         for (texto, linha, coluna) in botoes:
